6291061bf1ddc241b1c7762107a4f4c4bf1fcc85
Naman Pancholi <mahendra.naman07@gmail.com>
2023-10-20 01:50:02 +0530

    DONE

--- main.go
+++ main.go
@@ -42,9 +42,15 @@ func main() {
 func updateMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for _, item := range movies {
+	for index, item := range movies {
 		if item.ID == params["id"] {
-
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			_ = json.NewEncoder(w).Encode(movie)
+			return
 		}
 	}
 }
@@ -55,7 +61,7 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 	_ = json.NewDecoder(r.Body).Decode(&movie)
 	movie.ID = strconv.Itoa(rand.Intn(100000000))
 	movies = append(movies, movie)
-	_ = json.NewEncoder(w).Encode(movie)
+	_ = json.NewEncoder(w).Encode(movies)
 }
 
 func getMovie(w http.ResponseWriter, r *http.Request) {
